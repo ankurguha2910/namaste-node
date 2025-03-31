af4251f4a9292ad9b454c9c3206a10fc37673583
ankurguha2910 <ankur.guha16@gmail.com>
2025-03-31 16:21:18 +0530

    Thread pool working

---
+++ thread-pool.js
@@ -0,0 +1,23 @@
+const crypto = require("crypto");
+
+process.env.UV_THREADPOOL_SIZE = 8;
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+    console.log("1 - key generated");
+});
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+    console.log("2 - key generated");
+});
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+    console.log("3 - key generated");
+});
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+    console.log("4 - key generated");
+});
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+    console.log("5 - key generated");
+});
